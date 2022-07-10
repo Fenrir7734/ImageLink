9aa734e401ede9d4a06414736e55f106bcec3434
Fenrir7734 <hitex1999@gmail.com>
2022-07-10 11:40:18 +0200

    Init

---
+++ Backend/src/main/java/com/fenrir/imagelink/ImageLinkApplication.java
@@ -0,0 +1,13 @@
+package com.fenrir.imagelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageLinkApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/fenrir/imagelink/ImageLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fenrir.imagelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
