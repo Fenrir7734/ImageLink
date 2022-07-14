c96a8af3019963a171750ab87133f90bc78d8980
Fenrir7734 <hitex1999@gmail.com>
2022-07-14 17:10:23 +0200

    Renamed

--- Backend/src/main/java/com/fenrir/imagelink/service/RandomStringGeneratorService.java
+++ Backend/src/main/java/com/fenrir/imagelink/service/RandomStringGeneratorService.java
@@ -5,14 +5,13 @@ import org.springframework.stereotype.Service;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
-public class UrlGeneratorService {
-    private static char[] alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
-    private static int URL_LENGTH = 8;
+public class RandomStringGeneratorService {
+    private static final char[] alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
 
-    public String getRandomUrl() {
+    public String generate(int length) {
         StringBuilder builder = new StringBuilder();
 
-        for (int i = 0; i < URL_LENGTH; i++) {
+        for (int i = 0; i < length; i++) {
             int index = ThreadLocalRandom.current().nextInt(alphabet.length);
             builder.append(alphabet[index]);
         }
