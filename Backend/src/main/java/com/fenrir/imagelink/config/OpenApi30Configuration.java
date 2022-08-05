885e0e98e5ab5bf63b7080a9d45cd18a5e2aa963
Fenrir7734 <hitex1999@gmail.com>
2022-08-05 14:26:11 +0200

    Added swagger

---
+++ Backend/src/main/java/com/fenrir/imagelink/config/OpenApi30Configuration.java
@@ -0,0 +1,9 @@
+package com.fenrir.imagelink.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "ImageLink Spring Rest API", version = "v1.0"))
+public class OpenApi30Configuration { }
