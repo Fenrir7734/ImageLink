fca9f05cef422b603d2acfbf06b1e0dfcfc69c6a
Fenrir7734 <hitex1999@gmail.com>
2023-02-05 20:04:58 +0100

    Create landing view module

--- Backend/src/main/java/com/fenrir/imagelink/config/OpenApi30Configuration.java
+++ Backend/src/main/java/com/fenrir/imagelink/config/OpenApi30Configuration.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * http://localhost:8080/swagger-ui/index.html
+ */
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "ImageLink Spring Rest API", version = "v1.0"))
 public class OpenApi30Configuration { }
